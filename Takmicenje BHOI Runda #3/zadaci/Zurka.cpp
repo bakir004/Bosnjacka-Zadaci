7a13a4f5719ca2013dba4322ab700bd60d7733f6
bakir004 <bakeroni11@gmail.com>
2021-05-31 13:52:22 +0100

    Kvalifikacije Runda #3

--- Takmicenje BHOI Runda #3/zadaci/Zurka.cpp
+++ Takmicenje BHOI Runda #3/zadaci/Zurka.cpp
@@ -5,65 +5,8 @@
 #include <queue>
 
 using namespace std;
-// map<int, bool> visited;
-// map<int, bool> visited2;
-// map<int, bool> friendOfGoodFriend;
-// map<int, bool> isGoodFriend;
-// map<int, bool> hasGoodFriend;
 
-// map<int, vector<int>> goodFriendGraph;
 map<int, vector<int>> graph;
-
-// void bfs(int src) {
-//     visited[src] = true;
-//     queue<int> q;
-//     int counter = 0;
-//     q.push(src);
-
-//     while(!q.empty()) {
-//         int curr = q.front();
-//         q.pop();
-
-//         if(graph[curr].size() >= 2) {
-//             isGoodFriend[curr] = true;
-//             goodFriendGraph[curr] = {};
-//         } else {
-//             return;
-//         }
-
-//         for (int i = 0; i < graph[curr].size(); i++) {
-//             if(!visited[graph[curr][i]]) {
-//                 visited[graph[curr][i]] = true;
-//                 // if(graph[graph[curr][i]].size() >= 2) {
-//                 //     isGoodFriend[graph[curr][i]] = true;
-//                 // }
-//                 if(graph[graph[curr][i]].size() >= 2) {
-//                     q.push(graph[curr][i]);
-//                     goodFriendGraph[curr].push_back(graph[curr][i]);
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// bool checkIfGood(int src, int _counter) {
-//     visited[src] = true;
-//     int counter = _counter;
-//     if(graph[src].size() >= 2) {
-//         for (int i = 0; i < graph[src].size(); i++) {
-//             if(!visited[graph[src][i]] && checkIfGood(graph[src][i], 1)) {
-//                 counter++;
-//             }
-//         }
-//         if(counter >= 2) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     } else {
-//         return false;
-//     }
-// }
 
 void removeSingleNodes() {
     queue<int> q;
@@ -111,20 +54,6 @@ int main() {
         }
     }
     cout << counter;
-    // int maxFriends = -1;
-    // for (int i = 0; i < m; i++) {
-    //     if(!visited[i])
-    //         bfs(i);
-    // }
-
-    // for(auto i : isGoodFriend) {
-    //     if(i.second == true && !visited2[i.first] == true) {
-    //         int totalFriends = bfs2(i.first);
-    //         maxFriends = max(totalFriends, maxFriends);
-    //     }
-    // }
-    
-    // cout << maxFriends;
 
     return 0;
 }
